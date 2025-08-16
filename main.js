c18cf3479534e923888a6265c97b4d62f9c6adbb
David Llanos <davidllanos.compina@gmail.com>
2025-08-16 12:06:28 -0500

    📖 DOC:

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 // const miweb = require('./modules/myModules')
 // console.log(miweb)
 
-const math = require('./math/index.js');
-// console.log(math);
-console.log(math.dividir(6, 3));
+// const math = require('./math/index.js');
+// // console.log(math);
+// console.log(math.dividir(6, 3));
+
+// let edad = 2
+// let mensaje
+
+// if ( edad >= 20 ){
+//     console.log("Eres mayor de edad")
+// } else {
+//     console.log("Eres menor de edad")
+// }
+
+// const edad = 34
+// const mensaje = edad >= 20 ? "Eres mayor de edad" : "Eres menor de edad"
+// console.log(mensaje)
+
+// const rol = ["jefe" , "administrador"]
+// const privilegios = rol.includes("administrador") ? "tiene acceso total" : "no tiene acceso total"
+// console.log(`El usuario ${rol} ${privilegios}`)
